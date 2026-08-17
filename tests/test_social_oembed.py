28ae0bbae85abfaa222ff9447f6930de5dc26f49
Uchenna Orji <bethuelorji@gmail.com>
2026-08-17 10:56:10 +0100

    Deleted test_manuel and added tests script for testing the class and functions. Starting with the SocialOEmbed class.

---
+++ tests/test_social_oembed.py
@@ -0,0 +1,32 @@
+from pub_oembed.social_media.linkedin import LinkedInOEmbed
+from pub_oembed.social_media.social_oembed import SocialOEmbed
+from pub_oembed.social_media.tiktok import TikTokOEmbed
+from pub_oembed.social_media.youtube import YouTubeOEmbed
+from pub_oembed.social_media.x_twitter import XTwitterOEmbed
+
+
+# test instance of SocialOEmbed
+def test_instance():
+    social = SocialOEmbed()
+    assert isinstance(social, SocialOEmbed)
+# -------------------------------
+
+
+# Inheritance checks
+def test_linkedin_inheritance():
+    assert issubclass(LinkedInOEmbed, SocialOEmbed)
+
+
+def test_tiktok_inheritance():
+    assert issubclass(TikTokOEmbed, SocialOEmbed)
+
+
+def test_youtube_inheritance():
+    assert issubclass(YouTubeOEmbed, SocialOEmbed)
+
+def test_x_tiwtter_inheritance():
+    assert issubclass(XTwitterOEmbed, SocialOEmbed)
+
+# --------------------------------------------
+
+
